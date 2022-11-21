73b538df968478ce5c3bc783c4209ae3dab31d85
DanielaSpivak <96259265+DanielaSpivak@users.noreply.github.com>
2022-11-21 17:18:42 +0200

    board class - update Stage class

--- src/main/java/com/knights_move/model/Board.java
+++ src/main/java/com/knights_move/model/Board.java
@@ -7,14 +7,17 @@ public class Board {
     private int width;
     private List<Tile> visitedTile;
     private List<Tile> emptyTile;
-    //private HashMap<Position, TypeTile> positionOfTiles; -> in game
+    private HashMap<Integer,HashMap<Position,Tile>> tilesPositionInBoard;
+    private HashMap<Position,Tile> tilePositions;
 
-    public Board(int boardId, int height, int width, List<Tile> visitedTile, List<Tile> emptyTile) {
+    public Board(int boardId, int height, int width, List<Tile> visitedTile, List<Tile> emptyTile , HashMap<Integer,HashMap<Position,Tile>> tilesPositionInBoard, HashMap<Position, Tile> tilePositions) {
         this.boardId = boardId;
         this.height = height;
         this.width = width;
         this.visitedTile = new ArrayList<>();
         this.emptyTile = new ArrayList<>();
+        this.tilesPositionInBoard = new HashMap<>();
+        this.tilePositions = new HashMap<>();
 
     }
 
@@ -25,6 +28,12 @@ public class Board {
     public void setBoardId(int boardId) {
         this.boardId = boardId;
     }
+
+
+    public HashMap<Integer, HashMap<Position, Tile>> getTilesPositionInBoard() {
+        return tilesPositionInBoard;
+    }
+
 
     public int getHeight() {
         return height;
@@ -45,6 +54,7 @@ public class Board {
     public List<Tile> getVisitedTile() {
         return Collections.unmodifiableList(visitedTile);
     }
+
 
     /*public void setVisitedTile(List<Tile> visitedTile) {
         this.visitedTile = visitedTile;
@@ -95,6 +105,51 @@ public class Board {
         }
         else
             return false;
+    }
+
+    public HashMap<Position, Tile> getTilePositions() {
+        return tilePositions;
+    }
+    //all tiles positions in each stage
+    public boolean addTilePosition(Position p, Tile t){
+        if(p != null && t != null && !tilePositions.containsKey(p))
+        {
+            //position range of x -> 0 till 63, y -> 0 till 63.
+            if(p.getX() < 64 && p.getY() <64) {
+                tilePositions.put(p,t);
+                return true;
+            }
+            else
+                return false;
+        }
+        else
+            return false;
+    }
+
+    public boolean removeTilePosition(Position p){
+        if(p != null && tilePositions.containsKey(p)) {
+            tilePositions.remove(p);
+            return true;
+        }
+        return false;
+    }
+
+    // for each board that belong to stage in game ->  HashMap<Integer,HashMap<Position,Tile>> tilesPositionInBoard
+    public boolean addTilesPositionToBoard (Position p, Tile t){
+        if(!tilesPositionInBoard.containsKey(boardId)) {
+            if(p != null && t != null){
+                boolean result = addTilePosition(p,t);
+                if(result){
+                    tilesPositionInBoard.put(boardId, tilePositions);
+                    return true;
+                }
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+        return false;
     }
 
 }
--- src/main/java/com/knights_move/model/Stage.java
+++ src/main/java/com/knights_move/model/Stage.java
@@ -5,13 +5,16 @@ public class Stage {
     private int stageNum;
     private int typeOfStage;
     private int  numOfForgottenTiles;
-    private HashMap<Position,Tile> tilePositions;
 
-    public Stage(int stageNum, int typeOfStage, int numOfForgottenTiles, HashMap<Position, Tile> tilePositions) {
+
+
+
+    public Stage(int stageNum, int typeOfStage, int numOfForgottenTiles) {
         this.stageNum = stageNum;
         this.typeOfStage = typeOfStage;
         this.numOfForgottenTiles = numOfForgottenTiles;
-        this.tilePositions = new HashMap<>();
+
+
     }
 
     public int getStageNum() {
@@ -21,6 +24,9 @@ public class Stage {
     public void setStageNum(int stageNum) {
         this.stageNum = stageNum;
     }
+
+
+
 
     public int getTypeOfStage() {
         return typeOfStage;
@@ -38,30 +44,9 @@ public class Stage {
         this.numOfForgottenTiles = numOfForgottenTiles;
     }
 
-    public HashMap<Position, Tile> getTilePositions() {
-        return tilePositions;
-    }
-    //all tiles positions
-    public boolean addTilePosition(Position p, Tile t){
-        if(p != null && t != null && !tilePositions.containsKey(p))
-        {
-            //position range of x -> 0 till 63, y -> 0 till 63.
-            if(p.getX() < 64 && p.getY() <64) {
-                tilePositions.put(p,t);
-                return true;
-            }
-            else
-                return false;
-        }
-        else
-            return false;
-    }
 
-    public boolean removeTilePosition(Position p){
-        if(p != null && tilePositions.containsKey(p)) {
-            tilePositions.remove(p);
-            return true;
-        }
-        return false;
-    }
+
+
+
+
 }
