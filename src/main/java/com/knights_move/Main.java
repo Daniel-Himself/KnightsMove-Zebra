41f953d77311f97cab276fcd2b8f6ae4c6373d21
Daniel <danielsharon97@gmail.com>
2022-11-28 13:02:54 +0200

    Reverted changes done to the Main class

--- src/main/java/com/knights_move/Main.java
+++ src/main/java/com/knights_move/Main.java
@@ -1,29 +1,9 @@
 package com.knights_move;
 
 import com.knights_move.view.HelloApplication;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
-
-// Main class runs an application
-public class Main extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/view/fxml/LoginPage.fxml"));
-            Scene scene = new Scene(root,1050,600);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        HelloApplication.main(args);
     }
 }
