cfc5d4f45decd78aefba9ad55c028be22d672a96
DanielaSpivak <96259265+DanielaSpivak@users.noreply.github.com>
2022-11-29 13:29:05 +0200

    main class - update

--- src/main/java/com/knights_move/Main.java
+++ src/main/java/com/knights_move/Main.java
@@ -1,23 +1,19 @@
 package com.knights_move;
 
-import com.knights_move.model.Question;
-import com.knights_move.model.SysData;
 import com.knights_move.view.HelloApplication;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+
+// Main class runs an application
 public class Main {
+
+
     public static void main(String[] args) {
         HelloApplication.main(args);
-        SysData instance= new SysData();
-        System.out.println(instance.DesJsonQuestions());
-        HelloApplication.main(args);
-        System.out.println( Question.fromJson2());
-
-
-        // ArrayList<Answer> array=new ArrayList<>();
-        //array.add(new Answer("answer1"));
-        //array.add(new Answer("answer1"));
-        //array.add(new Answer("answer1"));
-        //array.add(new Answer("answer1"));
-        // Question questions=new Question("q3",);
     }
 }
