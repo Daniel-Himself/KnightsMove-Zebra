e1bf1aefc640afe4edf086652b2f98668bcd0352
DanielaSpivak <96259265+DanielaSpivak@users.noreply.github.com>
2022-12-09 17:19:18 +0200

    .

--- src/main/java/com/knights_move/ui_controllers/PieChartController.java
+++ src/main/java/com/knights_move/ui_controllers/PieChartController.java
@@ -62,7 +62,7 @@ public class PieChartController extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         launch(args);
-    }
+    }*/
 }
