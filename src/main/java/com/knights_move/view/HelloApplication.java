a800a7066be0088e2f37014d5bbd2b27f8f784b5
Daniel <danielsharon97@gmail.com>
2022-12-13 18:49:56 +0200

    Changed initial fxml page to MainFrame

--- src/main/java/com/knights_move/view/HelloApplication.java
+++ src/main/java/com/knights_move/view/HelloApplication.java
@@ -1,18 +1,14 @@
 package com.knights_move.view;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import javafx.stage.WindowEvent;
 
 import java.io.IOException;
-import java.net.URL;
 
 public class HelloApplication extends Application {
     public static Scene scene;
@@ -22,7 +18,7 @@ public class HelloApplication extends Application {
     public void start(Stage primaryStage) {
 
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("QA.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("MainFrame.fxml"));
             parent = loader.load();
             scene = new Scene(parent, 800,600);
             scene.getStylesheets().add(getClass().getResource("design.css").toExternalForm());
