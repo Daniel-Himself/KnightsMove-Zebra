1b31b34d35ab1c2a3ddd79e2bcc174b070abcad7
Noa Assayag <Noa.assayag02@gmail.com>
2023-01-02 16:53:45 +0200

    kng'move method

--- src/main/java/com/knights_move/model/King.java
+++ src/main/java/com/knights_move/model/King.java
@@ -1,5 +1,6 @@
 package com.knights_move.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class King extends Figure implements FigureInterface{
@@ -9,10 +10,76 @@ public class King extends Figure implements FigureInterface{
     //need to complete
     @Override
     public Position move(Position horseCuPosition, Position kingCuPosition) {
+        List<Position> potentialMoves= new ArrayList<>();
+        int colX=kingCuPosition.getX();
+        int rowY=kingCuPosition.getY();
 
+        int colHorse=horseCuPosition.getX();
+        int rowHorse=horseCuPosition.getY();
 
-//        System.out.println("king");
-//        Position position = null; //TODO need to complete
-      return kingCuPosition;
+        ArrayList<Integer> optionForRows= new ArrayList<>();
+        ArrayList<Integer> optionForCols=new ArrayList<>();
+
+        double minDistance=1000;
+        Position closerHourse=new Position(1000,10000);
+
+        switch (rowY) {
+            case 0: {
+                optionForRows.add(0);
+                optionForRows.add(7);
+                optionForRows.add(1);
+                break;
+            } case  (7): {
+                optionForRows.add(0);
+                optionForRows.add(7);
+                optionForRows.add(6);
+                break;
+            } default: {
+                optionForRows.add(rowY);
+                optionForRows.add(rowY + 1);
+                optionForRows.add(rowY - 1);
+            }
+        }
+        switch (colX) {
+            case (0): {
+                optionForCols.add(0);
+                optionForCols.add(7);
+                optionForCols.add(1);
+                break;
+            } case (7): {
+                optionForCols.add(0);
+                optionForCols.add(7);
+                optionForCols.add(6);
+                break;
+            } default: {
+                optionForCols.add(colX);
+                optionForCols.add(colX + 1);
+                optionForCols.add(colX - 1);
+            }
+        }
+        for(int i=0;i<optionForRows.size();i++)
+        {
+            for (int j=0;j<optionForCols.size();j++)
+            {
+                if(!(optionForRows.get(i)==rowY&&optionForCols.get(j)==colX))
+                potentialMoves.add(new Position(optionForCols.get(j),optionForRows.get(i)));
+            }
+        }
+
+        for(Position position:potentialMoves)
+        {
+            int x=Math.abs(position.getX()-colHorse);
+            int y=Math.abs(position.getY()-rowHorse);
+
+            double currentDis=Math.sqrt(Math.abs(y*y-x*x));
+
+            if (Double.valueOf(currentDis)<Double.valueOf(minDistance))
+            {
+                minDistance=currentDis;
+                closerHourse.setX(position.getX());
+                closerHourse.setY(position.getY());
+            }
+        }
+      return closerHourse;
     }
 }
